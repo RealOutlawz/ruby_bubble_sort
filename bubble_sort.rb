0c37e479dccc712e6b4f475b996994730a4a3a28
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-17 20:49:17 -0500

    Implemented initial bubble_sort method structure

---
+++ bubble_sort.rb
@@ -0,0 +1,13 @@
+# Build a method #bubble_sort that takes an array and returns a sorted array. 
+
+def bubble_sort(array)
+    #sorted_array = []
+    array.each_with_index do |value, index|
+      
+    end
+  puts array
+end
+
+
+bubble_sort([4,3,78,2,0,2])
+# Example Output:[0,2,2,3,4,78] (just retunrs array sorted )
