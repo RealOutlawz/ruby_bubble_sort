c035426e739dba5bc602c198fa3037c85fe6813a
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-18 12:36:04 -0500

    Refactored bubble_sort method to remove unnecessary variables and improved code clarity

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,29 +1,22 @@
-# Build a method #bubble_sort that takes an array and returns a sorted array. 
-
 def bubble_sort(array)
-  array_end = array.length - 1
-  p array_end
+  array_end = array.length - 1 # necessary to avoid out of bounds comparison error
+  sorted_array = array.sort 
+
   loop do
-    swapped = false
-    (0...array_end).each do |index|
-    p index 
-       if array[index] > array[index+1]
-        p "sucess! : #{array}"
-        temp = array[index]
+    (0...array_end).each do |index| # loops from 0 to the end of the array
+       if array[index] > array[index+1] # if the value at the current index is greater than the one after it the values are swapped
+        temp = array[index] 
         array[index] = array[index+1]
         array[index+1] = temp
-        swapped = true
-        p "changed to : #{array}"
        end
-
     end 
-
-    break unless swapped == false
+    break if array == sorted_array # loop ends when the array is sorted
   end
-
   p array
 end
 
-
-bubble_sort([4,3,78,2,0,2])
-# Example Output:[0,2,2,3,4,78] (just print sorted array)
+bubble_sort([4, 3, 78, 2, 0, 2])
+bubble_sort([5, 3, 8, 4, 2])
+bubble_sort([10, -2, 3, 7, 1])
+bubble_sort([4, 4, 4, 4])
+bubble_sort([])
