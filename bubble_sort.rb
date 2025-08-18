dcc4edc967f9d6723facc2639ee30f0aaed21030
RealOutlawz <155398128+RealOutlawz@users.noreply.github.com>
2025-08-17 21:20:42 -0500

    Refactored bubble_sort method to improve sorting logic and output format

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,12 +2,19 @@
 
 def bubble_sort(array)
     #sorted_array = []
-    array.each_with_index do |value, index|
-      
+    (array[0]...array.length).each_with_index do |value, index|
+      current_value = array[index]
+      next_value = array[index+1]
+      if current_value > next_value
+        temp = next_value
+        array[index+1] = current_value
+        array[index] = temp
+      end
+  
     end
-  puts array
+  print array
 end
 
 
 bubble_sort([4,3,78,2,0,2])
-# Example Output:[0,2,2,3,4,78] (just retunrs array sorted )
+# Example Output:[0,2,2,3,4,78] (just print sorted array)
